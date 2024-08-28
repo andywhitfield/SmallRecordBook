133553558489470d3a2bb74f1a6c66d83988876d
Andy Whitfield <andy@andywhitfield.me.uk>
2024-08-28 09:55:40 +0100

    Highlight current and overdue reminders

---
+++ SmallRecordBook.Web/Pages/ViewModels/CssExtensions.cs
@@ -0,0 +1,20 @@
+using SmallRecordBook.Web.Models;
+
+namespace SmallRecordBook.Web.Pages.ViewModels;
+
+public static class CssExtensions
+{
+    public static string ListItemCss(this RecordEntry recordEntry)
+    {
+        List<string> styles = ["srb-list-item"];
+        if (recordEntry.ReminderDate != null)
+        {
+            var today = DateOnly.FromDateTime(DateTime.UtcNow);
+            if (recordEntry.ReminderDate.Value == today)
+                styles.Add("srb-list-item-due");
+            if (recordEntry.ReminderDate.Value < today)
+                styles.Add("srb-list-item-overdue");
+        }
+        return string.Join(' ', styles);
+    }
+}
